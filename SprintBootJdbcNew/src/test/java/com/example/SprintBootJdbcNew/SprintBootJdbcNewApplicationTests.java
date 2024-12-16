448789f128a0326a18754e276189736bd428a972
lalitaSav <96169821+lalitaSav@users.noreply.github.com>
2024-12-16 23:23:44 +0530

    Add files via upload

---
+++ SprintBootJdbcNew/src/test/java/com/example/SprintBootJdbcNew/SprintBootJdbcNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SprintBootJdbcNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootJdbcNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
